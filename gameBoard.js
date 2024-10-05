84b9fbc2d41a218e6725a19f596a98617ce61b04
Insilises <penguin21492@gmail.com>
2024-10-05 12:45:16 -0500

    Functions to return vertical and horizontal lines work

--- gameBoard.js
+++ gameBoard.js
@@ -1,7 +1,13 @@
 function returnGameBoardVertices(minX, minY, maxX, maxY) {
+    //The board is split into three sections
+    var xSectionSize = (maxX - minX)/3;
+    var ySectionSize = (maxY - minY)/3;
+
     //TODO: Actually use these functions to create the tic tac toe board
-    var gameBoardVertices = returnVerticalLineVertices(minX, minY, maxY);
-    gameBoardVertices = gameBoardVertices.concat(returnHorizontalVertices(minY, minX, maxX));
+    var gameBoardVertices = returnVerticalLineVertices(minX + xSectionSize, 
+        minY, maxY);
+    gameBoardVertices = gameBoardVertices.concat(returnHorizontalVertices(minY + ySectionSize,
+        minX, maxX));
     return gameBoardVertices;
 }
 
@@ -14,8 +20,10 @@ function returnVerticalLineVertices(xValue, minY, maxY) {
 }
 
 function returnHorizontalVertices(yValue, minX, maxX) {
+    var width = maxX - minX;
     var lineVerticesPoints = [yValue, minX];
-    for (i=0; i < maxX; i++)
+    for (i=0; i < width; i++)
         lineVerticesPoints.push(minX + i, yValue);
+    console.log(lineVerticesPoints); //Testing
     return lineVerticesPoints;
 }
