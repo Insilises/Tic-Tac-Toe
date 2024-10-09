950fbe3bee37b9bb0f462045d3baf080c642e682
Insilises <penguin21492@gmail.com>
2024-10-09 16:32:56 -0500

    The circle can be placed anywhere on the canvas

--- OPiece.js
+++ OPiece.js
@@ -1,3 +1,11 @@
+/**The vertices to draw a circle using only points on anywhere
+ * on the canvas
+ * 
+ * @param {*} midPointX The X value in the middle of the circle
+ * @param {*} midPointY The Y value in the middle of the circle
+ * @param {*} radius The radius of the circle
+ * @returns adjustedOVertices
+ */
 function returnOPieceVertices(midPointX, midPointY, radius) {
     /*Pseudocode: 
     Find first point and other points using symmetry
@@ -8,9 +16,9 @@ function returnOPieceVertices(midPointX, midPointY, radius) {
         Find the other points using symmetry and already found point
     */
     
-    var oVertices = [midPointX, midPointY + radius]; //First point
-
-    //This loop works.
+    var oVertices = [0, radius]; //First point
+    
+    //Main loop to create the circle at a midpoint of (0,0)
     var pk = 5/4 - radius;
     for (i=0; oVertices[2*i] < oVertices[2*i + 1]; i++) {
         if (pk < 0) {   //Stay on the same row 
@@ -22,37 +30,38 @@ function returnOPieceVertices(midPointX, midPointY, radius) {
             pk = pk + (2 * oVertices[2*i] + 2) - 2 * (oVertices[2*i + 1] +1);
         }
     }
-
+    
+    //Add symmetrical points around the circle
     var symmetricPoints = returnSymmetricPointsOnCircle(oVertices[0], oVertices[1], midPointX, midPointY);
     for (i=0; i < oVertices.length; i++)
         symmetricPoints = symmetricPoints.concat(returnSymmetricPointsOnCircle(oVertices[2*i], oVertices[2*i + 1], midPointX, midPointY));
-    
-    console.log("Symmetric = " + symmetricPoints);
-    console.log("oVert = " + oVertices);
-    
     oVertices = oVertices.concat(symmetricPoints);
-    return oVertices;
+
+    //Adjust the vertices so they are somewhere around the canvas
+    var adjustedOVertices = [oVertices[0] + midPointX, oVertices[1] + midPointY];
+    for (i=1; i < oVertices.length; i++)
+        adjustedOVertices.push(oVertices[2*i] + midPointX, oVertices[2*i + 1] + midPointY);
+    return adjustedOVertices;
 }
 
-/**Given one point and the midpoint of a circle, return eight points, all calculated
- * based on the fact that circles are symmetrical.
+/**Given one point in the top left of quadrant 1 and the midpoint of a circle, return seven more
+ * points, all calculated based on the fact that circles are symmetrical.
  * 
  * @param {*} pointX 
  * @param {*} pointY 
  * @param {*} midPointX 
  * @param {*} midPointY 
- * @returns 
+ * @param {*} radius
+ * @returns symmetricVertices
  */
 function returnSymmetricPointsOnCircle(pointX, pointY, midPointX, midPointY) {
-    /*TODO: I'll have to change some things to make it so the answer depends on midpoint
-    You know, so we can make circles that aren't dead center in the screen. I need the
-    symmetry to work first before I can do that. */
+
 
     //Quadrant 1
     //Quad one top left is already pointX, pointY; Don't plot it again
     var symmetricVertices = [pointY, pointX]; //Quad one bottom right
 
-    
+
     //Quad 4
     symmetricVertices.push(pointY, -pointX); //Quad 4 top right
     symmetricVertices.push(pointX, -pointY); //Quad 4 bottom left
@@ -66,9 +75,7 @@ function returnSymmetricPointsOnCircle(pointX, pointY, midPointX, midPointY) {
     //Quad 2
     symmetricVertices.push(-pointY, pointX); //Quad 2 bottom left
     symmetricVertices.push(-pointX, pointY); //Quad 2 top right
-    
+    //TODO: Lots to do; maybe generate around 0,0, but we then use midpoints
 
-
-    //console.log("SymVert = " + symmetricVertices);
     return symmetricVertices;
 }
